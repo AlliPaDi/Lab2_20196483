d7e9e738000b9e91db9f653879359857327973ee
Allison <a20196483@pucp.edu.pe>
2024-09-07 04:54:53 -0500

    Funcionamiento del appbar

--- app/src/main/java/com/example/telegame/TelegameActivity.java
+++ app/src/main/java/com/example/telegame/TelegameActivity.java
@@ -2,13 +2,18 @@ package com.example.telegame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
 
 public class TelegameActivity extends AppCompatActivity {
 
@@ -19,11 +24,17 @@ public class TelegameActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_telegame);
 
+        // botón de retroceso
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Obtener el Intent que inició esta actividad
-        Intent intent = getIntent();
+//        Intent intent = getIntent();
         // Obtener el nombre del jugador
-        String nombre = intent.getStringExtra("nombre");
+//        String nombre = intent.getStringExtra("nombre");
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -32,4 +43,26 @@ public class TelegameActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+
+    // App Bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+    // Ir a Estadisticas
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.estadisticas) {
+            // Inicia la Activity de Estadísticas
+            Intent intent = new Intent(this, Estadisticas.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
